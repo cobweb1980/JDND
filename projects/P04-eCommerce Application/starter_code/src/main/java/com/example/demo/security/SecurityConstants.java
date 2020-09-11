e2a49bcd6213456b5f5766f9a863e9a9b1900bd7
Fejlesztes <fejlesztes@ctk.hu>
2020-09-11 17:31:43 +0200

    JWT integration and Sanity tests added

---
+++ projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/security/SecurityConstants.java
@@ -0,0 +1,9 @@
+package com.example.demo.security;
+
+public class SecurityConstants {
+
+    public static String com.auth0.samples.authapi.springbootauthupdated.security.SecurityConstants.EXPIRATION_TIME;
+import static com.auth0.samples.authapi.springbootauthupdated.security.SecurityConstants.HEADER_STRING;
+import static com.auth0.samples.authapi.springbootauthupdated.security.SecurityConstants.SECRET;
+import static com.auth0.samples.authapi.springbootauthupdated.security.SecurityConstants.TOKEN_PREFIX;
+}
